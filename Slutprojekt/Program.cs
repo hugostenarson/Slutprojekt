1992b802966fdc9dd65620eb3eeb3401cb6a0a6b
hugostenarson <hugo.stenarson@elev.ga.ntig.se>
2024-03-25 09:29:06 +0100

    hej

--- Slutprojekt/Program.cs
+++ Slutprojekt/Program.cs
@@ -16,10 +16,12 @@ int xPos = generator.Next(20, 780);
 
 Texture2D fruitBasket = Raylib.LoadTexture("fruitbasket.png");
 Texture2D banana = Raylib.LoadTexture("banana.png");
-Raylib_cs.Rectangle basketRect = new Raylib_cs.Rectangle(360, 565, 40, 20);
-Raylib_cs.Rectangle bananaRect = new Raylib_cs.Rectangle(xPos, 10, 10, 5);
 Raylib_cs.Rectangle barrierL = new Raylib_cs.Rectangle(-1, 0, 1, 600);
 Raylib_cs.Rectangle barrierR = new Raylib_cs.Rectangle(795, 0, 1, 600);
+Raylib_cs.Rectangle barrierBot = new Raylib_cs.Rectangle(0, 600, 800, 1);
+Raylib_cs.Rectangle basketRect = new Raylib_cs.Rectangle(360, 565, 40, 20);
+Raylib_cs.Rectangle bananaRect = new Raylib_cs.Rectangle(xPos, 10, 10, 5);
+
 
 Vector2 fall = new Vector2(0, 3);
 
@@ -37,6 +39,7 @@ if (Raylib.CheckCollisionRecs(basketRect, bananaRect))
 {
     bananaRect.y = -20;
     bananaRect.x = generator.Next(20, 780);
+    Raylib.DrawText("+1", 750, 20, 30, Raylib_cs.Color.RED);
 }
 
 
@@ -50,6 +53,12 @@ if (Raylib.CheckCollisionRecs(basketRect, barrierL))
 if (Raylib.CheckCollisionRecs(basketRect, barrierR))
 {
     basketRect.x = 740;
+}
+
+if (Raylib.CheckCollisionRecs(bananaRect, barrierBot))
+{
+    bananaRect.y = -20;
+    bananaRect.x = generator.Next(20, 780);
 }
 
 
@@ -75,9 +84,11 @@ bananaRect.y += fall.Y;
 
 Raylib.BeginDrawing();
     Raylib.ClearBackground(Raylib_cs.Color.LIGHTGRAY);
+    Raylib.DrawRectangleRec(barrierL, Raylib_cs.Color.LIGHTGRAY);
+    Raylib.DrawRectangleRec(barrierR, Raylib_cs.Color.LIGHTGRAY);
+    Raylib.DrawRectangleRec(barrierBot, Raylib_cs.Color.BLACK);
     Raylib.DrawTexture(banana, (int)bananaRect.x, (int)bananaRect.y, Raylib_cs.Color.WHITE);
     Raylib.DrawTexture(fruitBasket, (int)basketRect.x, (int)basketRect.y, Raylib_cs.Color.WHITE);
-    Raylib.DrawRectangleRec(barrierL, Raylib_cs.Color.LIGHTGRAY);
-    Raylib.DrawRectangleRec(barrierR, Raylib_cs.Color.BLACK);
+    Raylib.DrawText("SCORE:");
 Raylib.EndDrawing();
 }
